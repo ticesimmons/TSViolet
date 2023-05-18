574e84beb702e78de2f7be32d5e45282b03a8fc1
Tice <ticesimmons@gmail.com>
2023-05-18 00:47:14 -0500

    doing some initial testing with the robot class

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,7 @@ module JavafxSceneBuilder {
 	requires javafx.fxml;
 	requires org.apache.poi.poi;
 	requires org.apache.poi.ooxml;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
